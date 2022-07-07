14f00cd7a45932ed4c22ca76c069e04f103e51a0
kola <adedokun271@gmail.com>
2022-07-07 06:51:47 +0100

    make it better

--- 1688api2.py
+++ 1688api2.py
@@ -5,14 +5,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-
+import selenium.webdriver
 
 import time
 import sys
 import json
 from bs4 import BeautifulSoup
 import os
-driver= webdriver.PhantomJS()
+driver= selenium.webdriver.PhantomJS()
 url = 'https://detail.1688.com/offer/650635541901.html?spm=a260k.dacugeneral.home2019rec.6.6633436cLYnbqX&tracelog=p4p&clickid=b7ad1f4f0aff41bca346974460c0cbf1&sessionid=134b5a5d503cc164298709a41b8b3775'
 #url = 'https://detail.1688.com/offer/649838054631.html'
 
