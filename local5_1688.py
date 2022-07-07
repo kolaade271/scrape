7b4096e6de0dc79c1d8b1ff30d024e28ffb7f32b
kola <adedokun271@gmail.com>
2022-07-07 07:15:32 +0100

    make it better

--- local5_1688.py
+++ local5_1688.py
@@ -24,14 +24,14 @@ def message():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-sh-usage")
-    options.add_argument("start-maximized")
-    options.add_argument("--auto-open-devtools-for-tabs")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option('useAutomationExtension', False)
-    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-    DRIVER_BIN = os.path.join(PROJECT_ROOT, "chromedriver")
-    driver = webdriver.Chrome(DRIVER_BIN)
-    #driver = webdriver.Chrome(executable_path= os.environ.get("CHROMEDRIVER PATH"), chrome_options=options)
+    # options.add_argument("start-maximized")
+    # options.add_argument("--auto-open-devtools-for-tabs")
+    # options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    # options.add_experimental_option('useAutomationExtension', False)
+    # PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+    # DRIVER_BIN = os.path.join(PROJECT_ROOT, "chromedriver")
+    # driver = webdriver.Chrome(DRIVER_BIN)
+    driver = webdriver.Chrome(executable_path= os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options)
     
     domain = urlparse(url).netloc
     if domain == "m.1688.com" or  domain == "detail.1688.com":
